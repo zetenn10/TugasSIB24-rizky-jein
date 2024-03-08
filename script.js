762dcfb4dfaa95b47a8044d3487d7dde5664533d
zetenn10 <zardhina2@gmai.com>
2024-03-08 14:01:03 +0700

    Update Tugas P6 JavaScript

---
+++ script.js
@@ -0,0 +1,196 @@
+// alert('Hello World');
+
+
+/*
+alert('Hello World'); console.log('statement baris kedua');
+
+console.log('statement 1 baris')
+*/
+
+
+// // Komentar 1 baris
+// alert('Hello World'); console.log('statement baris kedua');
+
+// /*
+// Komentar multi
+// baris
+// */
+// console.log('statement 1 baris')
+
+
+/*
+var fullName = "Rizky Jein Nur Aulia"
+
+document.write(fullName)
+*/
+
+
+
+/*
+var fullName = "Rizky Jein Nur Aulia"
+
+fullName = "Nanami Kento"
+
+fullName = "Yamada Akito"
+
+document.write(fullName)
+*/
+
+
+/*
+var fullName = "Rizky Jein Nur Aulia"
+
+fullName = "Nanami Kento"
+
+fullName = "Yamada Akito"
+
+document.write(fullName)
+document.write('<br>')
+document.write(fullName)
+document.write('<br>')
+document.write(fullName)
+document.write('<br>')
+document.write(fullName)
+document.write('<br>')
+document.write(fullName)
+document.write('<br>')
+document.write(fullName)
+document.write('<br>')
+document.write(fullName)
+document.write('<br>')
+document.write(fullName)
+*/
+
+/*
+let fullName = "Rizky Jein Nur Aulia"
+
+fullName = "Iky"
+
+document.write(fullName)
+*/
+
+
+/*
+const fullName = "Rizky Jein Nur Aulia"
+
+fullName = "Iky"
+
+document.write(fullName)
+*/
+
+
+/*
+var x = 10
+if (true) {
+    var x = 20;
+    console.log("Nilai x di dalam blok:", x);
+}
+console.log("Nilai x di luar blok", x)
+*/
+
+/*
+let y = 10;
+if (true) {
+    let y = 20;
+    console.log("Nilai x di dalam blok:", y);
+}
+console.log("Nilai x di luar blok", y)
+*/
+
+
+/*
+let bulat = 25
+
+let desimal = 25.2
+
+document.write(bulat)
+document.write('<br>')
+document.write(desimal)
+document.write('<br>')
+document.write("Hasil Penjumlahan = ")
+document.write(desimal + bulat)
+*/
+
+
+
+let bulat = 25 
+let string = 'a'
+
+document.writeln(bulat/0)
+document.writeln('<br>')
+document.writeln(string/5)
+
+
+/*
+let bulat = 12312321432423423534534534523424234234n;
+
+document.writeln(bulat)
+*/
+
+
+/*
+let text = "Rizky Jein";
+
+document.writeln(text)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
