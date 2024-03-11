e1219abadbd460941cd6471632a2eec7afd04099
zetenn10 <zardhina2@gmai.com>
2024-03-11 17:06:22 +0700

    Update JavaScript2

--- script.js
+++ script.js
@@ -112,14 +112,14 @@ document.write(desimal + bulat)
 */
 
 
-
+/*S
 let bulat = 25 
 let string = 'a'
 
 document.writeln(bulat/0)
 document.writeln('<br>')
 document.writeln(string/5)
-
+*/
 
 /*
 let bulat = 12312321432423423534534534523424234234n;
@@ -135,62 +135,246 @@ document.writeln(text)
 */
 
 
+/*
+let admin = true
+let perbandingan = 15 > 9
+
+document.writeln(perbandingan)
+*/
+
+/*
+let admin 
+
+document.writeln(admin)
+*/
+
+
+/*
+let admin = null
+
+document.writeln(admin)
+*/
+
+
+/*
+let employee = Symbol('Iky')
+
+console.log(employee)
+console.log(employee.description)
+*/
+
+
+/*
+let employee = {
+    name: "Rizky Jein Nur Aulia",
+    age: 21,
+    job: "Full Stack Web Developer",
+    isMarried: false,
+}
+
+document.write(`My name is ${employee.name} and I'm ${employee.age} years old`)
+*/
+
+
+/*
+let value = false
+let string = String(value)
+alert(typeof value)
+*/
+
+
+/*
+let str = "123"
+let nbr = Number("123")
+
+alert(typeof nbr)
+*/
+
+
+/*
+alert(Boolean(0))
+alert(Boolean(1))
+*/
+
+
+/*
+let a = 3
+let b = 5
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a ** b);
+console.log(a / b);
+console.log(a % b);
+*/
+
+
+/*
+let a = 3
+let b = 5
+
+console.log(a == b);
+console.log(a < b);
+console.log(a > b);
+console.log(a /= b);
+*/
+
+
+/*
+let a = 3
+let b = 5
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+*/
+
+
+/*
+let a = 3
+let b = 5
+
+console.log(a & b);
+console.log(a | b);
+console.log(a ^ b);
+console.log(~a);
+*/
+
+
+/*
+let a = 3
+let b = 5
+
+console.log(a == b ? 'sama' : 'tidak sama')
+*/
+
+
+/*
+alert('Halo, selamat datang di website Fundamental JavaScript!')
+*/
+
+
+/*
+let string = prompt("Apakah anda ingin pergi amakn malam bersama saya?")
+alert('Jawaban Anda ' + string)
+*/
+
+
+/*
+let string = confirm("Apakah anda ingin pergi amakn malam bersama saya?")
+
+string ? alert('Jawaban Anda mau') : alert('Jawaban Anda tidak mau')
+*/
+
+
+/*
+let string = confirm("Apakah anda ingin pergi amakn malam bersama saya?")
+
+if (string) {
+    alert('Jawaban Anda mau')
+}
+*/
+
+
+/*
+let string = confirm("Apakah anda ingin pergi amakn malam bersama saya?")
+
+if (string) {
+    alert('Jawaban Anda mau')
+} else {
+    alert('Jawaban Anda tidak')
+}
+*/
+
+
+/*
+let string = prompt("1 + 1 = ?")
+
+if (string == 2) {
+    alert('Jawaban Anda benar')
+}else if (string >= 2) {
+    alert('Jawaban Anda kelebihan')
+}else if (string <= 2) {
+    alert('Jawaban Anda kurang')
+}else {
+    alert('Jawaban Anda salah')
+}
+*/
+
+
+/*
+let string = "blue"
+
+switch (string) {
+    case "red":
+        alert("I love red!")
+        break;
+    
+    case "blue":
+        alert("I love blue!")
+        break;
+
+    default:
+        alert("I don't know what the color is it!")
+}
+*/
+
+
+/*
+for (let i = 1; i < 5; i++) {
+    document.writeln(i)
+    document.writeln("<br>")
+}
+*/
+
+
+/*
+let b = 1
+
+while (b < 5) {
+    document.writeln(b)
+    document.writeln("<br>")
+    b++
+}
+*/
+
+
+/*
+let i = 1;
+
+document.writeln('Perulangan Do-While')
+document.writeln("<br>")
+do {
+    document.writeln(i)
+    document.writeln("<br>")
+    i++
+} while (i < 5);
+*/
 
 
 
+document.writeln('Perulangan For')
+document.writeln("<br>")
+
+for (let a = 1; a < 5; a++) {
+    if (a ==3) {
+        break
+    }
+    document.writeln(a)
+    document.writeln("<br>")
+}
 
 
+/*
+document.writeln('Perulangan For')
+document.writeln("<br>")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for (let a = 1; a < 5; a++) {
+    if (a == 3) {
+        continue
+    }
+    document.writeln(a)
+    document.writeln("<br>")
+}
+*/
